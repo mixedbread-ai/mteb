ccac133a456cf1387a605d2a926a35234b3cc300
Sean Lee <xmlee97@gmail.com>
2024-04-18 12:17:06 +0800

    register miracl

--- mteb/tasks/Retrieval/__init__.py
+++ mteb/tasks/Retrieval/__init__.py
@@ -9,6 +9,7 @@ from .deu.GerDaLIRSmallRetrieval import *
 from .deu.GermanDPRRetrieval import *
 from .deu.GermanQuADRetrieval import *
 from .deu.LegalQuADRetrieval import *
+from .deu.GermanMiracl import *
 from .eng.AILACasedocsRetrieval import *
 from .eng.AILAStatutesRetrieval import *
 from .eng.ArguAnaRetrieval import *
